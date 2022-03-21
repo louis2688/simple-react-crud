0c79eb6ccefd0c4e79b846e0fd8fcf18e1e25ad7
louis.a.madrigal <louis.a.madrigal@accenture.com>
2022-03-21 22:00:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# React CRUD App 
+
+
+## Build Setup
+
+```bash
+# install project
+$ git clone 
+
+# open app directory
+$ cd react-crud-app
+
+# install dependencies
+$ npm i 
+
+# serve with hot reload at localhost
+$ npm run dev
+
+# build for production
+$ npm build
+```
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Styles
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <span className="copyright">&copy; KT SESSION 2</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
